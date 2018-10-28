ad56c4735037e84f02cfed1ec930987c5b5317af
edwarda3 <edwarda3@oregonstate.edu>
2018-10-27 17:12:22 -0700

    fixed deadlock issue

--- din_phil.c
+++ din_phil.c
@@ -45,8 +45,21 @@ void *phil(int n){
 		sleep(thinking_time);
 
 		// Get forks
-		pthread_mutex_lock(&eating_fork[n]);
-		pthread_mutex_lock(&eating_fork[(n+1)%5]);
+		/*
+		 * We get the forks at the phil's spot, then the +1 spot for everybody but the 5th philosopher.
+		 * This ensures no deadlock.
+		 * For instance, if philosopher 1,2,3,4 all grabbed their first fork at the exact same time, and were waiting for the second one, then 5 is already blocked to try and grab its n+1, or 1st fork. In this case, 4 can eat, and then unblock 3, then 2, etc. 
+		 * If before this, 5 grabbed its fork first (fork #1), that blocks philosopher 1 and 5 can eat, since it can get fork #5. If 4 had grabbed fork #4, then that means philosopher 4 is already eating since it can only grab fork 5 if it has fork 4.
+		 * In this way, every case possible, everybody cannot be blocked. If 4 are, then 1 is guaranteed to be eating already.
+		 * If the sleep() calls are changed to usleep to test, then you will find that it never stops, and therefore never deadlocks.
+		 */
+		if(n!=4){
+			pthread_mutex_lock(&eating_fork[n]);
+			pthread_mutex_lock(&eating_fork[(n+1)%5]);
+		} else {
+			pthread_mutex_lock(&eating_fork[(n+1)%5]);
+			pthread_mutex_lock(&eating_fork[n]);
+		}
 		forks[n] = n+1;
 		forks[(n+1)%5] = n+1;
 
